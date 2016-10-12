f5ce29e302038aa0cb4df10a4fbca815d221ce55
richelbilderbeek <richel@richelbilderbeek.nl>
2016-10-12 10:46:35 +0200

    Added demo mode

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <QApplication>
+#include <QTimer>
 #include "qtdialog.h"
 
 int main(int argc, char *argv[])
@@ -6,6 +7,14 @@ int main(int argc, char *argv[])
   QApplication a(argc, argv);
   QtDialog w;
   w.show();
+
+  //In demo mode, close after 1 second
+  if (argc == 2 && argv[1] == std::string("--demo"))
+  {
+    QTimer * const timer = new QTimer;
+    QObject::connect(timer, SIGNAL(timeout()), &w, SLOT(close()));
+    timer->start(1000);
+  }
   
   return a.exec();
 }
